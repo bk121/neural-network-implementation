11dc32002c8f47e2fde0727d8aa7783cfb17c43d
Jonas Birk <jonasbirk1996@gmail.com>
2022-03-01 16:58:18 +0000

    Temporarily remove dropout from part 1
    Former-commit-id: e37ab8ed39e98271a7a1d37ff16f59680438ea3f

--- part1_nn_lib.py
+++ part1_nn_lib.py
@@ -333,11 +333,11 @@ class MultiLayerNetwork(object):
         a = x
         for i, layer in enumerate(self._layers):
             # Implementing dropout
-            if i % 2 == 1:
-                binary_values = np.random.rand(a.shape[0], a.shape[1]) < (1 - self._dropout_rate)
-                a = layer.forward(a) * binary_values
-                a /= (1 - self._dropout_rate)
-            else:
+            # if i % 2 == 1:
+            #     binary_values = np.random.rand(a.shape[0], a.shape[1]) < (1 - self._dropout_rate)
+            #     a = layer.forward(a) * binary_values
+            #     a /= (1 - self._dropout_rate)
+            # else:
                 a = layer.forward(a)
         return a
 
