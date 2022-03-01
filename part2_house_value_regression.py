604f0c0061634f054e4b345cdceb0128792b97e3
Jonas Birk <jonasbirk1996@gmail.com>
2022-03-01 17:36:59 +0000

    Remove inspect import
    Former-commit-id: ffb028532f5b44dda6ef2e0dbb8dbad03c3c1105

--- part2_house_value_regression.py
+++ part2_house_value_regression.py
@@ -2,7 +2,7 @@ import random
 # from tkinter.tix import Y_REGION
 # from pytest import skip
 import torch
-import inspect
+# import inspect
 import pickle
 import numpy as np
 import pandas as pd
@@ -156,31 +156,33 @@ class Regressor():
         """Get parameter names for the estimator"""
         # fetch the constructor or the original constructor before
         # deprecation wrapping if any
-        init = getattr(cls.__init__, "deprecated_original", cls.__init__)
-        if init is object.__init__:
-            # No explicit constructor to introspect
-            return []
+        # init = getattr(cls.__init__, "deprecated_original", cls.__init__)
+        # if init is object.__init__:
+        #     # No explicit constructor to introspect
+        #     return []
 
-        # introspect the constructor arguments to find the model parameters
-        # to represent
-        init_signature = inspect.signature(init)
-        # Consider the constructor parameters excluding 'self'
-        parameters = [
-            p
-            for p in init_signature.parameters.values()
-            if p.name != "self" and p.kind != p.VAR_KEYWORD
-        ]
-        for p in parameters:
-            if p.kind == p.VAR_POSITIONAL:
-                raise RuntimeError(
-                    "scikit-learn estimators should always "
-                    "specify their parameters in the signature"
-                    " of their __init__ (no varargs)."
-                    " %s with constructor %s doesn't "
-                    " follow this convention." % (cls, init_signature)
-                )
-        # Extract and sort argument names excluding 'self'
-        return sorted([p.name for p in parameters])
+        # # introspect the constructor arguments to find the model parameters
+        # # to represent
+        # init_signature = inspect.signature(init)
+        # # Consider the constructor parameters excluding 'self'
+        # parameters = [
+        #     p
+        #     for p in init_signature.parameters.values()
+        #     if p.name != "self" and p.kind != p.VAR_KEYWORD
+        # ]
+        # for p in parameters:
+        #     if p.kind == p.VAR_POSITIONAL:
+        #         raise RuntimeError(
+        #             "scikit-learn estimators should always "
+        #             "specify their parameters in the signature"
+        #             " of their __init__ (no varargs)."
+        #             " %s with constructor %s doesn't "
+        #             " follow this convention." % (cls, init_signature)
+        #         )
+        # # Extract and sort argument names excluding 'self'
+        # print(sorted([p.name for p in parameters]))
+        # return sorted([p.name for p in parameters])
+        return ['activations', 'batch_size', 'dropout_rate', 'learning_rate', 'loss_fun', 'nb_epoch', 'neurons', 'x']
 
     def get_params(self, deep=True):
         """
