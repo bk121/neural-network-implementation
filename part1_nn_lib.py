65b8dcef2d4e0d2e1689fab3fca3e315b59c1c37
ben kirwan <bk121@ic.ac.uk>
2022-03-02 18:26:32 +0000

    335 commented
    Former-commit-id: e2ef1e9b8e3e17f13e2db775cf05b1f3d37fbec0

--- part1_nn_lib.py
+++ part1_nn_lib.py
@@ -332,10 +332,10 @@ class MultiLayerNetwork(object):
             # print(layer)
             # Implementing dropout
             if i % 2 == 1:
-                binary_values = np.random.rand(a.shape[0], a.shape[1]) < (1 - self._dropout_rate)
+                # binary_values = np.random.rand(a.shape[0], a.shape[1]) < (1 - self._dropout_rate)
                 a = layer.forward(a)
                 # b = a * binary_values
-                # a /= (1 - self._dropout_rate)
+                a /= (1 - self._dropout_rate)
             else:
                 a = layer.forward(a)
         return a
